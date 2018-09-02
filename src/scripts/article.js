2280542a26c54dfc6a0e4a793c91761b72fc4c5f
henrym2 <henrym2@tcd.ie>
2018-09-02 13:07:08 +0100

    Implemented VUE.js compnenet for articles.

--- src/scripts/article.js
+++ src/scripts/article.js
@@ -1,4 +1,4 @@
-Vue.component("article", {
+Vue.component("article-front", {
     props: ["article"],
     template: ' <div class="w3-card-4 w3-margin w3-white">\
     <img src="/Images/NMS background.jpg" alt="Nature" style="width:100%">\
@@ -10,7 +10,7 @@ Vue.component("article", {
       <p>{{ article.PostText }}</p>\
       <div class="w3-row">\
         <div class="w3-col m8 s12">\
-          <p><a href = "articles/{{ article.id }}"class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></a></p>\
+          <p><a :href="article.ID" class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></a></p>\
         </div>\
         <div class="w3-col m4 w3-hide-small">\
           <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span class="w3-tag">0</span></span></p>\
@@ -21,25 +21,34 @@ Vue.component("article", {
   });
   //Initialize the props associated with the components
   var app = new Vue({
-    el: "#app",
+    el: "#app2",
     data: function() {
       return {
         articles: [],
         loading: true,
-        errored: false
+        errored: false,
+        route: "/articles/"
       };
     },
+   /* computed: {
+      getURL: function(_article) {
+        return this.route+_article.ID;
+      }
+    },*/
+  
     mounted () {
         axios
-            .get(localhost/api/articles.json)
+            .get('/api/articles')
             .then(response => {
-                this.articles = response.data
+                this.articles = response.data.Posters
+                console.log(response.data.Posters)
             })
             .catch(error => {
-                console.log(error)
+                console.log(error);
                 this.errored = true;
             })
-            .finally(() => this.loading = false)
-    }
+            .finally(() => this.loading = false);
+    }, 
+  
 });
     
